9f2bbaebd888680c3340aeb764bdc10b35b52d7b
Michael Rastetter <github2@michaelrastetter.de>
2026-05-21 11:11:13 +0200

    Add ConsoleRenderer to main()

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -6,9 +6,16 @@ internal class Program
     {
         string settingsPath = args.Length > 0 ? args[0] : "settings.json";
         GameSettings settings = GameSettings.LoadFromJson(settingsPath);
-        settings.PrintSettings();  // for debugging purposes
+        // settings.PrintSettings();  // for debugging purposes
 
-        //SimulationEngine engine = new(settings);
-        //engine.UpdatePattern();
+        SimulationEngine engine = new(settings);
+        ConsoleRenderer renderer = new(engine, targetFps: 5);
+        renderer.Start();
+
+        while (true)
+        {
+            engine.UpdatePattern();
+            Thread.Sleep(100);  // slow down for testing
+        }
     }
 }
